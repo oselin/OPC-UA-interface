aba3ba8256ea2d643abf6ecf1fdeb3fa20dc40d0
oselin <pierfrancesco.oselin@gmail.com>
2022-08-02 22:51:13 +0200

    Adding settigns and uptime support

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from platform import platform
 import tkinter as tk
 import tkinter.font as font
 import psutil
 import platform
-
+import datetime
  
 NCOLUMNS = 9
 NROWS    = 30
@@ -19,6 +18,9 @@ BOTTOM_MENU_COLOR      = '#0066ff'
 SELECTED_FAMILY = None
 SELECTED_DEVICE = None
 
+ID    = 0
+TEMP  = 0
+CLOCK = 0
 
 dev = {'len':13,
        'MODBUS-RTU':['TC0451','TB3421','PO0187','ZJ3253'],
@@ -27,8 +29,22 @@ dev = {'len':13,
 }
 
 
+def onConfigureClick():
+    frame05.grid(column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS,row=2,columnspan=NCOLUMNS - 2*SIDEMENU_COLUMNS,rowspan=NROWS-1,sticky='NWSE')
+    frame05.grid_propagate(0)
+    #lab_dev.grid(row=0,column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS, columnspan=NCOLUMNS - 2*SIDEMENU_COLUMNS,rowspan=2,sticky='NWSE')
 
-def onDeviceFamilylick(name):
+    lab09  .grid(row=2,column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS,sticky='NW',padx=PAD_X,columnspan=3)
+    cbID   .grid(row=3,column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS,sticky='NWS',padx=PAD_X)
+    cbTemp .grid(row=4,column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS,sticky='NWS',padx=PAD_X)
+    cbClock.grid(row=5,column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS,sticky='NWS',padx=PAD_X)
+
+    tk.Button(root,text='Save and close',activebackground=SIDEMENU_COLOR,
+                    padx=0,pady=0,bd=0,command=lambda: update(SELECTED_DEVICE),
+                         bg = DEVICE_LIST_COLOR,highlightthickness=0
+                    ).grid(column=NCOLUMNS-1,row=NROWS-2,sticky='NWSE',columnspan=1,padx=PAD_X,pady=PAD_X)
+
+def onDeviceFamilyClick(name):
     global SELECTED_FAMILY
     frame03['bg'] = DEVICE_LIST_COLOR
     lab08['bg']   = DEVICE_LIST_COLOR
@@ -53,13 +69,21 @@ def coolBottomBar():
         labCPU['text'] = 'CPU usage: ' + str(cpu)
     else:
         labCPU['text'] = 'CPU usage:  ' + str(cpu)
-    if platform.platform() == 'Linux':
+    if platform.system() == 'Linux':
         labTEMP['text'] = 'CPU temperature: ' + str(psutil.sensors_temperatures()['coretemp'][1][1])
+    
+    uptime['text']  = '10d ' + datetime.datetime.now().strftime('%Hh %Mmin %Ssec')
     root.after(500,coolBottomBar)
 
 
 def update(device=0):
     if device:
+        frame05.grid_forget()
+        lab09  .grid_forget()
+        cbID   .grid_forget()
+        cbTemp .grid_forget()
+        cbClock.grid_forget()
+
         data00['text'] = 'Connection type: '
         data01['text'] = 'Data received:  '
         data02['text'] = 'Data converted: '
@@ -71,15 +95,13 @@ def update(device=0):
         data02v['text'] = '-  '
         data03v['text'] = '- '
         data04v['text'] = '- '
-
+        
         tk.Button(root,text='Configure parameters',activebackground=SIDEMENU_COLOR,
-                    padx=0,pady=0,bd=0,#command=lambda val=dev[name][i]: onDeviceClick(val),
+                    padx=0,pady=0,bd=0,command=onConfigureClick,
                          bg = DEVICE_LIST_COLOR,highlightthickness=0
                     ).grid(column=NCOLUMNS-1,row=NROWS-2,sticky='NWSE',columnspan=1,padx=PAD_X,pady=PAD_X)
 
-
-
-    root.after(10,update)
+        root.after(10,update)
 
 
 root = tk.Tk()
@@ -145,6 +167,10 @@ lab04 = tk.Label(root,text=str(dev['len']),bg=SIDEMENU_COLOR).grid(row=3,column=
 lab05 = tk.Label(root,text='Warnings: ',bg=SIDEMENU_COLOR,font=justBold).grid(row=4,column=0,sticky='NW',padx=PAD_X)
 lab06 = tk.Label(root,text='0 ',bg=SIDEMENU_COLOR).grid(row=4,column=1,sticky='NW')
 
+tk.Label(root,text='Uptime: ',bg=SIDEMENU_COLOR,font=justBold).grid(row=5,column=0,sticky='NW',padx=PAD_X)
+uptime = tk.Label(root,text='0 ',bg=SIDEMENU_COLOR)
+uptime.grid(row=5,column=1,sticky='NW')
+
 
 #==========DEVICE FAMILY LIST==========
 frame02 = tk.Frame(root, bg=DEVICE_FAMILY_COLOR)
@@ -159,7 +185,7 @@ lab07 = tk.Label(root,text='Detected protocols ',bg=DEVICE_FAMILY_COLOR,font=jus
 
 for i in range(1,len(dev.keys())):
     button_i = tk.Button(root,text=list(dev.keys())[i],activebackground=DEVICE_LIST_COLOR,
-                         padx=PAD_X,bd=0,command=lambda val=list(dev.keys())[i]: onDeviceFamilylick(val),
+                         padx=PAD_X,bd=0,command=lambda val=list(dev.keys())[i]: onDeviceFamilyClick(val),
                          bg = DEVICE_FAMILY_COLOR,highlightthickness=0, anchor='w'
     ).grid(column=SIDEMENU_COLUMNS,row=i,sticky='NWSE',columnspan=DEVICE_COLUMNS)
 
@@ -191,9 +217,9 @@ data01.grid(row=3,column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS,sticky='NW',padx=PAD_
 data01v = tk.Label(root,text='',bg=DETAILED_SECTION_COLOR,font=justBold)
 data01v.grid(row=3,column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS+1,sticky='NW',padx=PAD_X)
 data02  = tk.Label(root,text='',bg=DETAILED_SECTION_COLOR,font=justBold)
-data02.grid(row=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS,column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS,sticky='NW',padx=PAD_X) 
+data02.grid(row=4,column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS,sticky='NW',padx=PAD_X) 
 data02v = tk.Label(root,text='',bg=DETAILED_SECTION_COLOR,font=justBold)
-data02v.grid(row=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS,column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS+1,sticky='NW',padx=PAD_X)
+data02v.grid(row=4,column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS+1,sticky='NW',padx=PAD_X)
 
 data03  = tk.Label(root,text='',bg=DETAILED_SECTION_COLOR,font=justBold)
 data03.grid(row=5,column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS,sticky='NW',padx=PAD_X)
@@ -204,10 +230,26 @@ data04.grid(row=8,column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS,sticky='NW',padx=PAD_
 data04v = tk.Label(root,text='',bg=DETAILED_SECTION_COLOR,font=justBold)
 data04v.grid(row=9,column=SIDEMENU_COLUMNS+2*DEVICE_COLUMNS,sticky='NW',padx=PAD_X,columnspan=NCOLUMNS - 2*SIDEMENU_COLUMNS,rowspan=2)
 
+#=========SETTINGS PAGE=========
+frame05 = tk.Frame(root, bg=DETAILED_SECTION_COLOR)
+
+
+lab09 = tk.Label(root,text='Settings and custom queries ',bg=DETAILED_SECTION_COLOR,font=justBold)
+
+cbID = tk.Checkbutton(root, text='ID',variable=ID, onvalue=1, offvalue=0,bg=DETAILED_SECTION_COLOR,
+                    highlightthickness=0,activebackground=DETAILED_SECTION_COLOR)
+
+cbTemp = tk.Checkbutton(root, text='Temperature',variable=TEMP, onvalue=1, offvalue=0,bg=DETAILED_SECTION_COLOR,
+                    highlightthickness=0,activebackground=DETAILED_SECTION_COLOR)
+
+cbClock = tk.Checkbutton(root, text='Clock',variable=CLOCK, onvalue=1, offvalue=0,bg=DETAILED_SECTION_COLOR,
+                    highlightthickness=0,activebackground=DETAILED_SECTION_COLOR)
+
+
 #==========BOTTOM MENU==========
-frame05 = tk.Frame(root, bg=BOTTOM_MENU_COLOR)
-frame05.grid(column=0,row=NROWS-1,columnspan=NCOLUMNS,sticky='NWSE')
-frame05.grid_propagate(0)
+frame06 = tk.Frame(root, bg=BOTTOM_MENU_COLOR)
+frame06.grid(column=0,row=NROWS-1,columnspan=NCOLUMNS,sticky='NWSE')
+frame06.grid_propagate(0)
 
 labRAM = tk.Label(root,text='Ram usage: -',bg=BOTTOM_MENU_COLOR,fg='white')
 labRAM.grid(row=NROWS-1,column=NCOLUMNS-1,columnspan=1,sticky='NWS')
